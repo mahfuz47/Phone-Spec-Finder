2c3b0a192e858b30c518df23d032c993f81848d0
mahfuz47 <mahfuzshikder560@gmail.com>
2022-03-01 22:40:06 +0600

    detail section setup done

--- phone-spec.js
+++ phone-spec.js
@@ -18,19 +18,20 @@ const displaySearchResult = (phones) => {
         const div = document.createElement("div");
         div.classList.add("col");
         div.innerHTML = `
-        <div class="card h-100  border-3 rounded-3 " id="card">
-        <img src="${phone.image}" class="card-img-top w-100" alt="image of phone" />
-        <div class="card-body">
+        <div class="card border shadow-lg h-100" id="card">
+            <img src="${phone.image}" class="card-img-top w-100 border-radius" alt="image of phone" />
+            <div class="card-body">
             <p class="card-text fw-bold">
                 <span class="text-muted">Brand: ${phone.brand}</span>
             </p>
             <h5 class="card-title">${phone.phone_name}</h5>
+            </div>
+            <div class="card-button text-center text-white ">
+            <button onclick="displayPhoneDetails('${phone.slug}')" class="btn btn-outline-warning px-5 py-2 rounded-pill fw-bolder">
+            Details
+            </button>
         </div>
-        <div class="card-button text-center py-2 text-white">
-            <button onclick = "displayDetails(${phone.slug})" class="btn btn-outline-warning px-5 py-2 rounded-pill fw-bolder">
-      Details
-    </button>
-        </div>
+            
     </div>
         `;
         searchResult.appendChild(div);
@@ -38,16 +39,57 @@ const displaySearchResult = (phones) => {
 };
 
 // Detail Section
-const displayDetails = (phoneId) => {
-    const url = `
-    https://openapi.programming-hero.com/api/phone/${phoneId}
-    `;
-    console.log(url);
+
+const displayPhoneDetails = (phoneId) => {
+    // console.log(phoneId);
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
-        .then((res) => stringify(res.json()))
-        .then((data) => console.log(data));
+        .then((res) => res.json())
+        .then((data) => loadDetails(data.data));
 };
 
-// const displayPhoneDetails = (phone) => {
-//     let phoneDetails = document.getElementById("phone-details");
-// };
+const loadDetails = (data) => {
+    console.log(data);
+    let phoneDetails = document.getElementById("phone-details");
+    const mainFeatures = Object.keys(data.mainFeatures);
+    const mainFeaturesValue = Object.values(data.mainFeatures);
+    const div = document.createElement("div");
+    div.setAttribute("class", "row d-flex");
+    phoneDetails.textContent = "";
+    div.innerHTML = `
+    <h3 class="text-center fw-bold gy-3 text-white">PHONE DETAILS</h3>
+    <div class="col-sm-12 col-md-4 "><img class="w-100 details-image" src="${data.image}" alt="" /></div>
+    <div class="col-sm-12 col-md-6">
+    <table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>FEATURES</th>
+            <th>MAIN FEATURES</th>
+            
+            
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>${mainFeatures[0]}</td>
+            <td>${mainFeaturesValue[0]}</td>
+            
+           
+        </tr>
+        <tr>
+            <td>${mainFeatures[0]}</td>
+            <td>Peter</td>
+            
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>John</td>
+            
+        </tr>
+    </tbody>
+</table>
+    </div>
+    
+            `;
+    phoneDetails.appendChild(div);
+};
